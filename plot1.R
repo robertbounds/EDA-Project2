844c86d01e0b901372afa2c2f719bfe3e69cc4e0
robertbounds <robert.s.bounds@gmail.com>
2014-06-21 17:01:30 -0500

    comments

--- plot1.R
+++ plot1.R
@@ -2,25 +2,30 @@ plot1 <- function() {
         
         NEI <- readRDS("summarySCC_PM25.rds")
         
-        years       <- unique(NEI$year)
+        years <- unique(NEI$year)
+        
+        ## extract emissions data by year
         emissions99 <- NEI[which(NEI$year == 1999), ]$Emissions
         emissions02 <- NEI[which(NEI$year == 2002), ]$Emissions
         emissions05 <- NEI[which(NEI$year == 2005), ]$Emissions
         emissions08 <- NEI[which(NEI$year == 2008), ]$Emissions
         
+        ## apply sum to emissions data
         emissionTotals <- c(sum(emissions99),
                             sum(emissions02),
                             sum(emissions05),
                             sum(emissions08)
                             )
         
+        ## add years as names to emissions totals
         names(emissionTotals) <- years
         
-        png(file = "plot1.png", width = 480, height = 480)
+        png(file = "plot1.png", width = 720, height = 720)
         
+        ## plot emissionsTotals using barplot from base plottting system
         barplot(emissionTotals,
                 main = "Total U.S. PM2.5 Emissions",
-                ylab = "PM2.5 emitted, in tons",
+                ylab = "PM2.5 emissions, in tons",
                 xlab = "Year",
                 col = "seagreen3"
                 )
--- plot2.R
+++ plot2.R
@@ -2,13 +2,17 @@ plot2 <- function() {
         
         NEI <- readRDS("summarySCC_PM25.rds")
         
+        ## subset Baltimore City data from NEI using the fips code provided
         baltimoreCity <- NEI[which(NEI$fips == 24510), ]
         years         <- unique(baltimoreCity$year)
+        
+        ## subset the emissions data for each year from the Baltimore baltimore data
         emissions99   <- baltimoreCity[which(baltimoreCity$year == 1999), ]$Emissions
         emissions02   <- baltimoreCity[which(baltimoreCity$year == 2002), ]$Emissions
         emissions05   <- baltimoreCity[which(baltimoreCity$year == 2005), ]$Emissions
         emissions08   <- baltimoreCity[which(baltimoreCity$year == 2008), ]$Emissions
         
+        ## sum baltimore emissions data for each year
         baltimoreTotals <- c(sum(emissions99),
                              sum(emissions02),
                              sum(emissions05),
@@ -17,11 +21,11 @@ plot2 <- function() {
         
         names(baltimoreTotals) <- years
         
-        png(file = "plot2.png", width = 480, height = 480)
+        png(file = "plot2.png", width = 720, height = 720)
         
         barplot(baltimoreTotals,
                 main = "Total PM2.5 Emissions, Baltimore City",
-                ylab = "PM2.5 emitted, in tons",
+                ylab = "PM2.5 emissions, in tons",
                 xlab = "Year",
                 col = "seagreen3"
                 )
