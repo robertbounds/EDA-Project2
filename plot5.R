7c262b934b32df795de27a20f0a0e172231fe137
robertbounds <robert.s.bounds@gmail.com>
2014-06-21 17:38:29 -0500

    comments

--- plot5.R
+++ plot5.R
@@ -4,7 +4,7 @@ library(ggplot2)
 plot5 <- function() {
         
         ### A quick Google search for 'definition of motor vehicle'
-        ### brings up a definition:
+        ### brings up the definition:
         ###
         ###     "a road vehicle powered by an 
         ###      internal combustion engine; an automobile."
@@ -14,8 +14,8 @@ plot5 <- function() {
         NEI <- readRDS("summarySCC_PM25.rds")
         SCC <- readRDS("Source_Classification_Code.rds")
         
-        ### Baltimore City data from NEI
-        ### National motor vehicle emissions data from SCC
+        ### Subset Baltimore City data from NEI
+        ### Subset motor vehicle emissions data from SCC
         baltimoreCity <- NEI[which(NEI$fips == 24510), ]
         onroad        <- SCC[which(SCC$Data.Category == "Onroad"), ]
         
