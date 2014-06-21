495ef00496985601e7468f0e111506ac2b73549f
robertbounds <robert.s.bounds@gmail.com>
2014-06-21 17:36:18 -0500

    subsetting, comments and formatting

--- plot4.R
+++ plot4.R
@@ -6,33 +6,32 @@ plot4 <- function() {
         NEI <- readRDS("summarySCC_PM25.rds")
         SCC <- readRDS("Source_Classification_Code.rds")
         
-        ############################################################
-        ### Extract coal combustion-related emissions information
-        ############################################################
-        
+        ### Subset coal combustion information
+        ### I chose to first subset for coal in the fuel/source column
+        ### Then, I subset the coal data for a combustion process
         coal <- SCC[grep("Coal", SCC$SCC.Level.Three), ]
         coal <- coal[grep("Combustion", coal$SCC.Level.One), ]
+        
+        coal$EI.Sector <- gsub("Fuel Comb - ", "", coal$EI.Sector)
+        coal$EI.Sector <- gsub(" - Coal", "", coal$EI.Sector)
+        
+        ### this data being removed had such a tiny total emissions amount
+        ### that there was no visible color bar on the plot.
+        ### I removed this data because leaving it in created blanks in the
+        ### plot, disrupted the experience.
+        coal <- coal[-which(coal$EI.Sector == "Electric Generation - Other"), ]
 
         
-        ############################################################
-        ### Convert DFs to DTs, then merge data
-        ############################################################
-        
+        ### Convert DFs to DTs, then merge data on SCC
         coal <- data.table(coal, key = "SCC")
         NEI <- data.table(NEI, key = "SCC")
         mergedTable <- merge(coal, NEI, key = "SCC")
         
-        ############################################################
-        ### Set year as an ordered factor
-        ############################################################
-        
+        ### Set year as an ordered factor to assist x-axis grouping later
         mergedTable$year <- factor(mergedTable$year, 
                                    levels = c("1999", "2002", "2005", "2008"),
                                    ordered = TRUE)
         
-        ############################################################
-        ### Plot
-        ############################################################
         
         g <- ggplot(data = mergedTable,
                     mapping = aes(x = year, y = Emissions))
@@ -40,9 +39,9 @@ plot4 <- function() {
                        mapping = aes(fill = EI.Sector),
                        position = "dodge",
                        stat = "identity")
-        g <- g + ylab("Emissions, in tons")
+        g <- g + ylab("PM2.5 Emissions, in tons")
         g <- g + xlab("Year")
-        g <- g + ggtitle("Total U.S. Coal Combustion-related Emissions")
+        g <- g + ggtitle("U.S. Coal Combustion-related, PM2.5 Emissions")
         
         ggsave("plot4.png")
 }
